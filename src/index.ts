b6f7858c63684a333388a05ac830ed48d948db85
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-19 00:16:16 +0900

    feat: いったん searchParams チェック

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
+interface IResponse {
+  error: boolean;
+  message?: string;
+  data?: {
+    title?: string;
+    description?: string;
+    image?: string;
+    favicon?: string;
+  }
+}
+
+const makeResponse = (status: number, response: IResponse): Response => {
+  return new Response(JSON.stringify(response), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    status,
+  })
+}
+
 export default {
   async fetch(request, env, ctx): Promise<Response> {
+    const url = new URL(request.url)
+    const requestTo = url.searchParams.get('q')
+
+    if (!requestTo || !URL.canParse(requestTo)) {
+      return makeResponse(400, {
+        error: true,
+        message: "Bad Request"
+      })
+    }
+
     return new Response('Hello World!')
   },
 } satisfies ExportedHandler<Env>
