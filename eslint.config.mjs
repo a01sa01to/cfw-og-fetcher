9ae38a37bbac1fa0313b0cfec03c5bca70e86eef
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-19 00:57:05 +0900

    fix configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,13 @@ import myConfig from '@a01sa01to/eslint-config'
 
 export default [
   ...myConfig.default,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   { ignores: ['.yarn/*', '.pnp*', '**/dist/*', 'worker-configuration.d.ts'] },
 ]
